e721d05fdffb448e17cf84053a1c15a87b56aa03
olivia <olivia.drobot@student.unitbv.ro>
2022-11-20 17:24:20 +0200

    Added Form Navigation(Game Form and Profile Form)

--- TriviadorGame/TriviadorGame/GameWindow.cpp
+++ TriviadorGame/TriviadorGame/GameWindow.cpp
@@ -12,8 +12,18 @@ GameWindow::GameWindow(QWidget* parent) : QDialog(parent)
 	ui.UsernameProfileFrom->setText(QString::fromStdString(player.GetUserName()));
 	ui.EmailLabelProfileForm->setText(QString::fromStdString(player.GetUserEmail()));
 
+	ui.GameForm->setVisible(true);
+	ui.ProfileForm->setVisible(false);
+
+	connect(ui.EditButton, SIGNAL(clicked()), this, SLOT(EditButtonClicked()));
+	connect(ui.SaveButton, SIGNAL(clicked()), this, SLOT(SaveButtonClicked()));
 }
 
 GameWindow::~GameWindow()
 {
+}
+void GameWindow::ToggleForm(QWidget* disabledForm, QWidget* enabledForm)
+{
+	disabledForm->setVisible(false);
+	enabledForm->setVisible(true);
 }
--- TriviadorGame/TriviadorGame/GameWindow.h
+++ TriviadorGame/TriviadorGame/GameWindow.h
@@ -12,11 +12,16 @@ class GameWindow : public QDialog
 private:
 	Ui::GameWindow ui;
 	Player player;
+	void ToggleForm(QWidget* disabledForm, QWidget* enabledForm);
 
 
 public:
 	explicit GameWindow(QWidget* parent = nullptr);
 
 	~GameWindow();
+private slots:
+	
+	void EditButtonClicked() { ToggleForm(ui.GameForm, ui.ProfileForm); }
+	void SaveButtonClicked() { ToggleForm(ui.ProfileForm, ui.GameForm); }
 };
 
