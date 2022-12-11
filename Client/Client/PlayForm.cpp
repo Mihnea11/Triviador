89325925905ed12f4e1e85ea7113af277d114755
ravyyyy <88721275+ravyyyy@users.noreply.github.com>
2022-12-11 16:30:48 +0200

    Created Room Form

--- Client/Client/PlayForm.cpp
+++ Client/Client/PlayForm.cpp
@@ -4,16 +4,36 @@ void PlayForm::ToggleWidget(QWidget* disabledForm, QWidget* enabledForm)
 	disabledForm->setVisible(false);
 	enabledForm->setVisible(true);
 }
+
+void PlayForm::CreateButton()
+{
+	ui.RoomForm->setVisible(true);
+}
+
 PlayForm::PlayForm(QWidget* parent) : QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+	ui.centralwidget->setVisible(true);
+	ui.RoomForm->setVisible(false);
+
+	ui.RoomPlayers->addItems({ "Two Players", "Three Players", "Four Players" });
+
+	connect(ui.CreateRoomPlayForm, SIGNAL(clicked()), this, SLOT(CreateButtonClicked()));
 }
 
 PlayForm::PlayForm(const Player& player, QWidget* parent)
 {
 	ui.setupUi(this);
 
+	ui.centralwidget->setVisible(true);
+	ui.RoomForm->setVisible(false);
+
+	ui.RoomPlayers->addItems({ "Two Players", "Three Players", "Four Players" });
+
 	this->player = player;
+
+	connect(ui.CreateRoomPlayForm, SIGNAL(clicked()), this, SLOT(CreateButtonClicked()));
 }
 
 PlayForm::~PlayForm()
--- Client/Client/PlayForm.h
+++ Client/Client/PlayForm.h
@@ -7,18 +7,25 @@
 #include "Player.h"
 #include "LoginForm.h"
 #include "ui_PlayForm.h"
+
 class PlayForm : public QMainWindow
 {
 	Q_OBJECT
+
 private:
 	Ui::PlayFormClass ui;
 	void ToggleWidget(QWidget* disabledForm, QWidget* enabledForm);
 	Player player;
+
+	void CreateButton();
+
 public:
 	PlayForm(QWidget* parent = nullptr);
 	PlayForm(const Player& player, QWidget* parent = nullptr);
 	~PlayForm();
+
 private slots:
+	void CreateButtonClicked() { CreateButton(); }
 };
 
 
